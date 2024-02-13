33dbbdf2c4d5d29e20657c356b9a8506e72b0e8c
anayouness52@gmail.com <anayouness52@gmail.com>
2024-02-13 17:32:19 +0100

    656

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
+let x=1;
 function App() {
+ const [i, hhh] = useState(0);
   return (
-   
-    
       <main>
+        <h1>{i}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis atque officiis amet. Dignissimos alias nostrum magnam velit sequi, quod est tempore commodi nam soluta assumenda ex dolor! Fugiat, sint rerum!</p>
+      <br />
+      <button onClick={()=>hhh(x++)}>Click here</button>
       </main>
     
   );
