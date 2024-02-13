72da281e1e7f6022d04f9eca8ce9cb1f85029f97
anayouness52@gmail.com <anayouness52@gmail.com>
2024-02-13 17:30:35 +0100

    001

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+const Navbar = () => {
+    return ( 
+        
+        <header>
+          <nav className="navbar">
+            <ul>
+              <li className="active">
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Contact</a>
+              </li>
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">Service</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+     );
+}
+ 
+export default Navbar;
