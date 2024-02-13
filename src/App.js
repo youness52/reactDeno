f2c5f0d89ca21ae1b7f84d1ad12112ae23230140
anayouness52@gmail.com <anayouness52@gmail.com>
2024-02-13 17:41:07 +0100

    hzed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
-let x=1;
+let x = 1;
 function App() {
- const [i, hhh] = useState(0);
+  const [i, hhh] = useState(0);
   return (
-      <main>
-        <h1>{i}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis atque officiis amet. Dignissimos alias nostrum magnam velit sequi, quod est tempore commodi nam soluta assumenda ex dolor! Fugiat, sint rerum!</p>
+    <main>
+      <h1>{i}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis atque
+        officiis amet. Dignissimos alias nostrum magnam velit sequi, quod est
+        tempore commodi nam soluta assumenda ex dolor! Fugiat, sint rerum!
+      </p>
       <br />
-      <button onClick={()=>hhh(x++)}>Click here</button>
-      </main>
-    
+      <div className="btn">
+        <button onClick={() => hhh(x++)}>+</button>
+        <button onClick={() => hhh(x--)}>-</button>
+      </div>
+    </main>
   );
 }
 
